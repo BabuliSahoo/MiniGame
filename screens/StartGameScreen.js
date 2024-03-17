8cfd268685d129c8cd92bab22532bacbb87525aa
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-03-17 18:26:51 +0530

    first commit

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,66 @@
+import { TextInput, Button, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
+
+
+function StarGameScreen() {
+
+  const [enteredNumber,SetEnteredNumber] = useState('');
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+        <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+        <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default StarGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer:{
+    flexDirection:'row'
+  },
+  buttonContainer:{
+    flex:1
+  }
+});
