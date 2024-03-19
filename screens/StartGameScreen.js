4dccc6b298c073ac95e74f5511527e51cdfd7d52
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-03-20 00:54:15 +0530

    Added colors to constant file

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import { TextInput, Button, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
+import Colors from "../constants/colors";
 
 function StarGameScreen({onPickNumber}) {
   const [enteredNumber, SetEnteredNumber] = useState("");
@@ -58,7 +59,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#4e0329",
+    backgroundColor: Colors.primary700,
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
@@ -71,9 +72,9 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     fontSize: 32,
-    borderBottomColor: "#ddb52f",
+    borderBottomColor:Colors.accent500,
     borderBottomWidth: 2,
-    color: "#ddb52f",
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
