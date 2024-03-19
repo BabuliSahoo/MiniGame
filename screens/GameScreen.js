642443b95f8e788f58c51cf59c129a2056a6e117
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-03-20 02:47:17 +0530

    UI improvement

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,11 @@
-import { Text, View, StyleSheet,Alert } from "react-native";
+import { View, StyleSheet,Alert } from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 import { useState,useEffect } from "react";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
+import Card from "../components/ui/Card";
+import InstructionText from "../components/ui/InstructionText";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -58,8 +60,8 @@ function GameScreen({ userNumber,OnGameOver }) {
     <View style={styles.screen}>
       <Title>Opponent's View</Title>
       <NumberContainer>{currentGuess} </NumberContainer>
-      <View>
-        <Text>Higher or Lower?</Text>
+      <Card>
+        <InstructionText>Higher or Lower?</InstructionText>
         <View>
           <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
             +
@@ -68,7 +70,7 @@ function GameScreen({ userNumber,OnGameOver }) {
             -
           </PrimaryButton>
         </View>
-      </View>
+      </Card>
       {/*<View> LOG ROUNDS</View> */}
     </View>
   );
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,48 +2,58 @@ import { TextInput, Button, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import { useState } from "react";
 import Colors from "../constants/colors";
+import Title from "../components/ui/Title";
+import InstructionText from "../components/ui/InstructionText";
 
-function StarGameScreen({onPickNumber}) {
+function StarGameScreen({ onPickNumber }) {
   const [enteredNumber, SetEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
     SetEnteredNumber(enteredText);
-    console.log('numberInputHandler :: '+enteredText);
+    console.log("numberInputHandler :: " + enteredText);
   }
 
   function resetHandler() {
-    SetEnteredNumber('');
+    SetEnteredNumber("");
   }
 
-  function confirmInputHandler(){
+  function confirmInputHandler() {
     const chosenNumber = parseInt(enteredNumber);
     console.log(chosenNumber);
-    if(isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99){
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
       // Show alert ...
-      Alert.alert('Invalid Number','Number has to be a number between 1 to 99',[{text:'Okay',style:'destructive',onPress:resetHandler}]);
+      Alert.alert(
+        "Invalid Number",
+        "Number has to be a number between 1 to 99",
+        [{ text: "Okay", style: "destructive", onPress: resetHandler }]
+      );
       return;
     }
-    console.log('Valid Number!');
+    console.log("Valid Number!");
     onPickNumber(chosenNumber);
   }
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.numberInput}
-        maxLength={2}
-        keyboardType="number-pad"
-        autoCapitalize="none"
-        autoCorrect={false}
-        onChangeText={numberInputHandler}
-        value={enteredNumber}
-      />
-      <View style={styles.buttonsContainer}>
-        <View style={styles.buttonContainer}>
-          <PrimaryButton onPress={resetHandler}>Reset</PrimaryButton>
-        </View>
-        <View style={styles.buttonContainer}>
-          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+    <View style={styles.rootContainer}>
+      <Title>Guess My Number</Title>
+      <View style={styles.inputContainer}>
+        <InstructionText style={styles.InstructionText}>Enter a Number</InstructionText>
+        <TextInput
+          style={styles.numberInput}
+          maxLength={2}
+          keyboardType="number-pad"
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChangeText={numberInputHandler}
+          value={enteredNumber}
+        />
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={resetHandler}>Reset</PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+          </View>
         </View>
       </View>
     </View>
@@ -53,6 +63,11 @@ function StarGameScreen({onPickNumber}) {
 export default StarGameScreen;
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    marginTop: 100,
+    alignItems:'center'
+  },
   inputContainer: {
     justifyContent: "center",
     alignItems: "center",
@@ -72,12 +87,16 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     fontSize: 32,
-    borderBottomColor:Colors.accent500,
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
     color: Colors.accent500,
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  InstructionText: {
+    color:Colors.accent500,
+    fontSize:24
   },
   buttonsContainer: {
     flexDirection: "row",
